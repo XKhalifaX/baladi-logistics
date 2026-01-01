28175e8cd49eddd0d376ae1227ac1d62ef29800a
XKhalifaX <Khalifaplay@Hotmail.com>
2026-01-01 18:03:23 +0400

    Directory and scaffolding creation.

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Baladi Backend is running!');
+
+});
+app.listen(PORT, () => {
+    console.log(` Server is running on http://localhost:${PORT}`);
+    
+});
